771f9d11f8851a025a77497e99f5348de6a5e52c
krasotun <servrostov@gmail.com>
2022-08-26 00:19:16 +0300

    add fonts and linter

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+function App() {
+	return (
+		<div className="page__content">
+			<Header />
+			<Footer />
+		</div>
+	);
+}
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<h2>Header works</h2>
+	);
+}
+
+export default Header;
