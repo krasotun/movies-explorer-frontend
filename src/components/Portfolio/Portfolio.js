172800c4a803e103dab03b8e2b9daaff633a1163
krasotun <servrostov@gmail.com>
2022-08-29 21:23:09 +0300

    add Добавил портфолио

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import ArrowLink from '../ArrowLink/ArrowLink';
 
 function Portfolio() {
+	const portfolioData = [
+		{
+			title: 'Статичный сайт',
+			link: 'https://krasotun.github.io/how-to-learn/',
+			id: 1,
+		},
+		{
+			title: 'Адаптивный сайт',
+			link: 'https://krasotun.github.io/russian-travel/',
+			id: 2,
+		},
+		{
+			title: 'Статичный сайт',
+			link: 'https://krasotun.nomoredomains.sbs/',
+			id: 3,
+		},
+	];
+	const portfolioContent = portfolioData.map((item) => (
+		<li className="portfolio__item" key={item.id}>
+			<p className="portfolio__text">
+				{item.title}
+			</p>
+			<ArrowLink
+				link={item.link}
+			/>
+		</li>
+	));
 	return (
-		<div>
-			Portfolio works
-		</div>
+		<section className="portfolio">
+			<p className="portfolio__title">
+				Портфолио
+			</p>
+			<ul className="portfolio__links">
+				{portfolioContent}
+			</ul>
+		</section>
 	);
 }
 
