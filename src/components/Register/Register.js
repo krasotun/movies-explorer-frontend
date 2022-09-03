ce4ca83e6c7c648aa3a5f9aba0fb00e4e39e968c
krasotun <servrostov@gmail.com>
2022-09-03 17:00:25 +0300

    add forms

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import Sign from '../Sign/Sign';
 
 function Register() {
 	return (
-		<h2>Register works!</h2>
+		<Sign
+			title="Добро пожаловать!"
+			buttonText="Зарегистрироваться"
+			questionTitle="Уже зарегистрированы?"
+			bottomLink="/signin"
+			bottomLinkText="Войти"
+		>
+			<label className="sign-form__label" htmlFor="name">
+				Имя
+				<input
+					required
+					className="form__item sign-form__item"
+					id="name"
+					type="name"
+					name="mane"
+					placeholder="Марат"
+					value="Марат"
+				/>
+			</label>
+			<span className="sign-form__error"><p className="sign-form__error-text sign-form__error-text_hidden ">Что то пошло не так...</p></span>
+			<label className="sign-form__label" htmlFor="email">
+				E-mail
+				<input
+					required
+					className="form__item sign-form__item"
+					id="email"
+					type="email"
+					name="email"
+					placeholder="E-mail"
+					value=""
+				/>
+			</label>
+			<span className="sign-form__error"><p className="sign-form__error-text sign-form__error-text_hidden ">Что то пошло не так...</p></span>
+			<label className="sign-form__label" htmlFor="password">
+				Пароль
+				<input
+					required
+					className="form__item sign-form__item"
+					id="password"
+					type="password"
+					name="password"
+					placeholder="******"
+					value=""
+				/>
+			</label>
+			<span className="sign-form__error"><p className="sign-form__error-text ">Что то пошло не так...</p></span>
+		</Sign>
+
 	);
 }
 
--- src/components/Sign/Sign.js
+++ src/components/Sign/Sign.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../Logo/Logo';
+import Button from '../Button/Button';
 
-function Sign() {
+function Sign({
+	title, children, buttonText, questionTitle, bottomLink, bottomLinkText,
+}) {
 	return (
-		<h2>Sign works!</h2>
+		<section className="sign">
+			<form className="form sign-form">
+				<div>
+					<Logo />
+					<h2 className="sign-form__title">{title}</h2>
+					{children}
+				</div>
+				<div>
+					<Button
+						label={buttonText}
+						type="sign-form-submit"
+					/>
+					<div className="sign-form__link-block">
+						<p className="sign__question">{questionTitle}</p>
+						<Link to={bottomLink} className="sign__link">{bottomLinkText}</Link>
+					</div>
+				</div>
+			</form>
+		</section>
 	);
 }
 
